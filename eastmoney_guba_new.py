b34592419afec0d239e53e85d00d4223b5d39a1d
Barry Wang <wyc6794@gmail.com>
2020-06-18 10:32:45 +0800

    refactor: 使用selenium库爬取评论 fix: 修复了部分字段爬取错误的问题

--- eastmoney_guba_new.py
+++ eastmoney_guba_new.py
@@ -4,39 +4,47 @@ import time
 import csv
 import requests
 import json
+import random
+import traceback
 import re
 import os
+from selenium import webdriver
 from bs4 import BeautifulSoup
-from fake_useragent import UserAgent
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
 
 BASE_URL = 'http://guba.eastmoney.com'
+USER_BASE_URL = "https://i.eastmoney.com"
 
 # 字段定义：[吧名称，股票代码，作者，标题，类型，ID，点击量，评论数，发帖时间，URL]
 GENERAL_HEADER = ['forum', 'stockid', 'author', 'title', 'category', 'identity', 'views', 'comments', 'time', 'link']
 CATEGORY_MAP = {"资讯": "zixun", "研报": "yanbao", "问董秘": "askanswer", "公告": "gonggao", "热门": "hot", "": "discussion"}
+INFLUENCE_MAP = {"stars0": "0", "stars05": "1", "stars1": "2", "stars15": "3", "stars2": "4",
+                 "stars25": "5", "stars3": "6", "stars35": "7", "stars4": "8", "stars45": "9", "stars5": "10"}
 
 # 字段定义：[帖子ID，评论ID，标题，内容，用户ID，发帖时间，点赞数，是否为一楼]
 CONTENT_HEADER = ['threadID', 'replyID', 'title', 'content', 'UID', 'postTime', 'likes', 'isFirstPage']
-STOCK_FILE_GUBA = "./stock_data_"
-STOCK_URL_GUBA = "./stock_url_"
 TOTAL_PAGE_NUM = 10
 PAGE_STEP = 100
 
 TOTAL_COMMENTS = 0
 OVERRIDE = False
-COUNTER = 0
-THRESHOLD = 3
+THRESHOLD = 5
 OVERRIDE_LIMIT = 5
+boost = 5
 
-UA = UserAgent()
+whoops = int(time.time())
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(options=chrome_options, executable_path="D:/Python/Selenium_Chrome_Driver/chromedriver.exe")
+driver.set_page_load_timeout(3)
+driver.set_script_timeout(3)
 
-
-STOCK_THREAD_FILE = "./thread_data_"
-BASE_URLSOURCE = "stock_url_00000.txt"
-
-# soup.select("#zwconbody > div")[0].get_text().replace(u"\u3000", "\n")
-# soup.select("#zwlist")[0].get_text().strip()
-# soup.select("#zwlist > div.zwli.clearfix")[1].get("data-reply_like_count")
+MODE0 = 0
+MODE1 = 1
 
 
 class timeBasedWriter():
@@ -69,7 +77,7 @@ class timeBasedWriter():
     
     def writeRow(self, row, localTime):
         if self.checkTime(localTime):
-            print("Correct, Writing")
+            # print("Correct, Writing")
             if self.getSize() == 0:
                 self._writer.writerow(GENERAL_HEADER)
                 self._file.flush()
@@ -93,17 +101,23 @@ def toLocalTime(tgtTimeStamp):
     return time.strftime("%Y-%m-%d %H", time.localtime(tgtTimeStamp))
 
 
-def fetchForumLinks(forumID, VERBOSE=False):
-    global COUNTER, THRESHOLD
-    COUNTER, THRESHOLD = 0, 0
+def fetchForumLinks(forumID, THRESHOLD, mode, VERBOSE=False):
+    COUNTER = 0
     forumID = str(forumID)
     # currentTimeStamp = int(time.time())
     # endTimeStamp = currentTimeStamp - (currentTimeStamp % 86400)
     # startTimeStamp = endTimeStamp - 86400
     pageNum = 1
+    if os.path.exists("./"):
+        pass
+    if mode == MODE0:
+        hi = whoops - 300
+        # print(int(time.time()), hi)
+    else:
+        hi = (whoops - (whoops % 3600)) - 3600
     json_name = "./url_list/" + str(forumID) + ".json"
     if os.path.exists(json_name) and os.path.getsize(json_name) > 0:
-        print("Loading:", json_name)
+        # print("Loading:", json_name)
         json_file = open(json_name, "r+", encoding="utf-8")
         thread_history = json.load(json_file)
     else:
@@ -111,12 +125,13 @@ def fetchForumLinks(forumID, VERBOSE=False):
         thread_history = {}
     INIT_YEAR = 2020
     prevMonth = 13
+    flag = True
     writer = timeBasedWriter("./guba_general_data/guba_", toLocalTime(int(time.time())))
-    while True and (VERBOSE and COUNTER <= THRESHOLD):
+    while flag and COUNTER < THRESHOLD:
         url = BASE_URL + "/list," + forumID + "_" + str(pageNum) + ".html"
         print("Fetching URL:", url)
         COUNTER += 1
-        web_source = requests.get(url, headers={"User-Agent": UA.random})
+        web_source = requests.get(url)
         soup = BeautifulSoup(web_source.content.decode("utf-8"), "lxml")
         threadList = soup.select("#articlelistnew > div.normal_post")
         forumName = soup.select("#stockname > a")[0].get_text()
@@ -134,41 +149,58 @@ def fetchForumLinks(forumID, VERBOSE=False):
                     category = CATEGORY_MAP[category[0].get_text()]
 
                 link = details[2].select("a")[0].get("href")
+                identity = details[2].select("a")[0].get("href")[1:-5]
+                title = details[2].select("a")[0].get_text()
                 if link[0] != "h":
                     link = BASE_URL + link
+                else:
+                    identity = link.split("/")[-1][:-5]
+                if category == "askanswer":
+                    link = details[2].select("a")[1].get("href")
+                    link = BASE_URL + link
+                    title = details[2].select("a")[1].get_text()
+                    identity = link.split("/")[-1][:-5]
 
                 general_result = {"forum": forumName[:-1],
                                   # "stockid": re_mode.search(articleInfo[0].get("href")).group()
                                   "stockid": forumID,
                                   "author": details[3].select("a > font")[0].get_text(),
-                                  "title": details[2].select("a")[0].get_text(),
+                                  "title": title,
                                   "category": category,
-                                  "identity": details[2].select("a")[0].get("href")[1:-5],
+                                  "identity": identity,
                                   "views": details[0].get_text().strip(),
                                   "comments": details[1].get_text().strip(),
                                   "time": date,
                                   "link": link
                                   }
                 print(general_result)
+                if toTimeStamp(date + ":" + "00") < hi:
+                    flag = False
+                    break
+                prevMonth = int(date[5:7])
                 # filename1 = "./guba_general_data/general_" + date[-5:-2] + ".csv"
                 writer.writeRow(general_result.values(), date[:-3])
                 thread_history[general_result["identity"].split(",")[-1]] = general_result["link"]
-            print(pageNum)
+            # print(pageNum)
         else:
             json_file.write(json.dumps(thread_history))
             break
         pageNum += 1
-        time.sleep(1)
+        time.sleep(random.randint(1, boost))
     writer.close()
     json_file.write(json.dumps(thread_history))
+    json_file.close()
 
 
 def FetchBodyContent(inst, testFlag = False):
-    url = inst[:-5] + "_1" + ".html"
-    print(url)
-    body = requests.get(url, headers={"User-Agent": UA.random})
+    url = inst[:-5] + ".html"
+    print("Now Fetching:", url)
+    try:
+        driver.get(url)
+    except TimeoutException:
+        return
     # print(body.text)
-    soup = BeautifulSoup(body.content.decode("utf-8"), "lxml")
+    soup = BeautifulSoup(driver.page_source.encode("utf-8"), "lxml")
     # contentWriter = timeBasedWriter("./guba_body_data/guba_", toLocalTime(int(time.time())))
     if testFlag:
         print("Fetching URL:", url)
@@ -195,91 +227,124 @@ def FetchBodyContent(inst, testFlag = False):
                       }
         print(page_content)
         if not testFlag:
+            time.sleep(random.randint(1, boost))
             # contentWriter.writeRow(list(page_content.values()), page_content["postTime"][:-6])
-            with open("./guba_content_data/guba_content_" + page_content["postTime"][:-6] + ".csv", "a", encoding="utf-8") as csvfile:
-                writer = csv.writer(csvfile)
+            with open("./guba_content_data/guba_content_" + page_content["postTime"][:-6] + ".csv", "a",
+                      encoding="utf-8", newline="") as csvfile:
+                writer = csv.writer(csvfile, delimiter="ǁ")
                 writer.writerow(list(page_content.values()))
 
-
-            FetchReplies(inst, soup, page_content["threadID"], page_content["postTime"])
+            numcomments = int(soup.select("#stockheader > div > div > span.tc1.replyCount")[0].get_text())
+            if numcomments > 0:
+                # print("Total comments:", numcomments)
+                FetchReplies(inst, page_content["threadID"], page_content["postTime"])
+            else:
+                print("No comments, skipping.")
         else:
             print(page_content)
-            time.sleep(0.5)
+            time.sleep(random.randint(1, boost))
         global TOTAL_COMMENTS
         TOTAL_COMMENTS += 1
     else:
         if testFlag:
             print("URL:" + url + " DOES NOT EXIST, SKIPPING............")
     # contentWriter.close()
-    time.sleep(1)
 
 
-def FetchReplies(inst, soup, threadID, postTime, testFlag=False):
+def FetchReplies(inst, threadID, postTime, testFlag=False):
     global TOTAL_COMMENTS
-    hot_replies = soup.select("#comment_hot_content > div > div")
-    # replyWriter = timeBasedWriter("./guba_body_data/guba_content_", postTime)
-    print("Fetching Replies", len(hot_replies))
-    for r in range(len(hot_replies)):
-        hot_result = {"threadID": threadID,
-                      "replyID": hot_replies[r].get("data-reply_id"),
-                      "title": "",
-                      "content": hot_replies[r].select("div > div.level1_reply_cont > div.full_text")[0].get_text().strip(),
-                      "UID": hot_replies[r].select("div > div.replyer_info > a").get("data-popper"),
-                      "postTime": hot_replies[r].select("div > div.publish_time")[0].get_text()[4:23],
-                      "likes": hot_replies[r].get("data-reply_like_count") or "0",
-                      "isFirst": False,
-        }
-        print(hot_result)
-        if not testFlag:
-            with open("./guba_content_data/guba_content_" + hot_result["postTime"][:-6] + ".csv", "a", encoding="utf-8") as csvfile:
-                writer = csv.writer(csvfile, delimiter="ǁ")
-                writer.writerow(list(hot_result.values()))
-            # contentWriter.writeRow(list(hot_result.values()))
-            TOTAL_COMMENTS += 1
-        else:
-            print(hot_result)
     page_counter = 1
     while True:
         url = inst[:-5] + "_" + str(page_counter) + ".html"
         # print(url)
-        web_content = requests.get(url, headers={"User-Agent": UA.random})
-        soup = BeautifulSoup(web_content.content.decode("utf-8"), "lxml")
-        replies = soup.select("#comment_hot_content > div > div")
+        if page_counter != 1:
+            try:
+                driver.get(url)
+                WebDriverWait(driver, timeout=2).until(
+                    EC.visibility_of_any_elements_located((By.CSS_SELECTOR, "#comment_all_content > div > div")))
+            except TimeoutException:
+                print("NORMAL COMMENTS: Timeout, skipping")
+        replies = driver.find_elements_by_css_selector("#comment_all_content > div > div")
         num_comments = len(replies)
-        print("num_comments", num_comments)
+        print("Num of NORMAL Comments:", num_comments)
         if num_comments == 0:
             break
         else:
             for r in range(num_comments):
-                # print("-------"+str(r)+"-------")
-                contents = replies[r].select("div.zwlitx > div > div.zwlitext.stockcodec > div")
                 reply_result = {"threadID": threadID,
-                              "replyID": hot_replies[r].get("data-reply_id"),
-                              "title": "",
-                              "content": hot_replies[r].select("div > div.level1_reply_cont > div.full_text")[0].get_text().strip(),
-                              "UID": hot_replies[r].select("div > div.replyer_info > a").get("data-popper"),
-                              "postTime": hot_replies[r].select("div > div.publish_time")[0].get_text()[4:23],
-                              "likes": replies[r].get("data-reply_like_count") or "0",
-                              "isFirst": False,
-                              }
-                print(postTime, reply_result["postTime"])
-                print(toTimeStamp(reply_result[postTime]) - toTimeStamp(postTime) <= 432000)
+                                "replyID": replies[r].get_attribute("data-reply_id"),
+                                "title": "",
+                                "content": replies[r].find_element_by_css_selector("div > div.level1_reply_cont > \
+                                div.short_text").text.strip(),
+                                "UID": replies[r].find_element_by_css_selector("div > div.replyer_info > a")
+                                .get_attribute("data-popper"),
+                                "postTime":replies[r].find_element_by_css_selector("div > div.publish_time").text[4:23],
+                                "likes": replies[r].get_attribute("data-reply_like_count") or "0",
+                                "isFirst": False,
+                               }
+                # print(postTime, reply_result["postTime"])
+                print("Replied within 5 DAYS:", toTimeStamp(reply_result["postTime"]) - toTimeStamp(postTime) <= 432000)
                 print(reply_result)
-                if not testFlag and (toTimeStamp(reply_result[postTime]) - toTimeStamp(postTime) <= 432000):
+                if not testFlag and (toTimeStamp(reply_result["postTime"]) - toTimeStamp(postTime) <= 432000):
                     with open("./guba_content_data/guba_content_" + reply_result["postTime"][:-6] + ".csv", "a",
-                              encoding="utf-8") as csvfile:
+                              encoding="utf-8", newline="") as csvfile:
                         writer = csv.writer(csvfile, delimiter="ǁ")
                         writer.writerow(list(reply_result.values()))
+                        un = "./user_list/" + reply_result["postTime"][:-6] + ".json"
+                        if os.path.exists(un) and os.path.getsize(un) > 0:
+                            print("Loading:", un)
+                            ujf = open(un, "r+", encoding="utf-8")
+                            uh = json.load(ujf)
+                        else:
+                            ujf = open(un, "w", encoding="utf-8")
+                            uh = {}
+                        if uh.get(reply_result["UID"], -1) != -1:
+                            uh[reply_result["UID"]] += 1
+                        else:
+                            uh[reply_result["UID"]] = 1
+                        ujf.seek(0)
+                        print(uh)
+                        ujf.write(json.dumps(uh))
+                        ujf.close()
                     TOTAL_COMMENTS += 1
                 else:
-                    print(reply_result)
+                    pass
         page_counter += 1
-        time.sleep(1)
+        if num_comments < 30:
+            break
+        time.sleep(random.randrange(1, boost))
 
 
-def generalFetcher(forumList, VERBOSE=False):
+def fetchUserById(uid, ttt):
+    url = USER_BASE_URL + "/" + str(uid)
+    web_source = requests.get(url)
+    soup = BeautifulSoup(web_source.content.decode("utf-8"), "lxml")
+    views = soup.select("#others > div > div.others_top > div.others_content > div.others_info > p")[0].select("span")
+    user_data = {
+        "username": soup.select("#others > div > div.others_top > div.others_content > div.others_title > div.others_username")[0].get_text().strip(),
+        "influence": INFLUENCE_MAP[soup.select("#influ_star")[0].get("class")[0]],
+        "age": soup.select("#others > div > div.others_top > div.others_content > div.others_title > div.others_level > p")[1].select("span")[0].get_text()[:-1],
+        "following": soup.select("#tafollownav > p > span")[0].get_text(),
+        "fans": soup.select("#tafansa > p > span")[0].get_text(),
+        "total_view": views[0].get_text(),
+        "today_view": views[1].get_text()
+    }
+    with open("./user_data/user_" + ttt + ".csv", "a", encoding="utf-8", newline="") as csvfile:
+        writer = csv.writer(csvfile, delimiter="ǁ")
+        writer.writerow(list(user_data.values()))
+
+
+def ufsr(fn):
+    fn1 = "./user_list/" + fn
+    with open(fn1, "r") as f:
+        fjs = json.load(f)
+        for k in fjs.keys():
+            fetchUserById(k, fn[5:-4])
+
+
+def generalFetcher(forumList, mode, VERBOSE=False):
     for id in forumList:
-        fetchForumLinks(id, VERBOSE)
+        fetchForumLinks(id, THRESHOLD, mode, VERBOSE)
 
 
 def bodyFetcher(forumList):
@@ -291,8 +356,20 @@ def bodyFetcher(forumList):
             # print(json_dic)
             for urls in json_dic.values():
                 FetchBodyContent(urls)
+        json_file.close()
 
 
 if __name__ == "__main__":
-    generalFetcher(forumList=[603986], VERBOSE=True)
-    bodyFetcher(forumList=[603986])
+    try:
+        sf = open("stockid.csv", "r", encoding="utf-8")
+        lissy = csv.reader(sf)
+        sli = list(lissy)[0][:100]
+        generalFetcher(forumList=sli, mode=MODE0, VERBOSE=False)
+        bodyFetcher(forumList=sli)
+        for ul in os.listdir("./user_list"):
+            ufsr(ul)
+            
+    except Exception as err:
+        print(err)
+        print(traceback.print_exc())
+        driver.quit()
